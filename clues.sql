c2914eb4a718a67440793dd7244a5a9100a0815d
melissaceee <cheng.melissa@outlook.com>
2024-11-06 12:59:43 -0500

    question2

--- clues.sql
+++ clues.sql
@@ -10,6 +10,7 @@ LIMIT 1;
 
 -- Write SQL query here
 SELECT language, isofficial
+FROM countrylanguages WHERE countrycode = 'VAT';
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
